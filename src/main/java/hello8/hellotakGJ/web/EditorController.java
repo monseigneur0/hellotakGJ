17bf6670b8bd77c61ead7c62020b4adaa23ac5bc
monseigneur0 <82815356+monseigneur0@users.noreply.github.com>
2022-11-09 22:50:49 +0900

    new home start

--- src/main/java/hello8/hellotakGJ/web/EditorController.java
+++ src/main/java/hello8/hellotakGJ/web/EditorController.java
@@ -5,15 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/editor")
 public class EditorController {
 
     @GetMapping("/new1")
     public String new1() {
         return "simpleide/new1";
     }
-    @GetMapping("/new2")
+    @GetMapping("/")
     public String new2() {
         return "simpleide/flat";
+    }
+
+    @GetMapping("/new2")
+    public String new3() {
+        return "simpleide/new2";
     }
 }
