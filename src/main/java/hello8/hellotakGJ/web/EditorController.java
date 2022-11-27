54610f0b1ebb41659458496ae3582584c0a75fe7
monseigneur0 <82815356+monseigneur0@users.noreply.github.com>
2022-11-27 16:26:53 +0900

    flat editor2 tailwind

--- src/main/java/hello8/hellotakGJ/web/EditorController.java
+++ src/main/java/hello8/hellotakGJ/web/EditorController.java
@@ -11,13 +11,25 @@ public class EditorController {
     public String new1() {
         return "simpleide/new1";
     }
-    @GetMapping("/")
-    public String new2() {
+    @GetMapping("/flat")
+    public String newflat() {
         return "simpleide/flat";
     }
 
     @GetMapping("/new2")
-    public String new3() {
+    public String new2() {
         return "simpleide/new2";
+    }
+    @GetMapping("/")
+    public String new31() {
+        return "simpleide/new3";
+    }
+    @GetMapping("/new3")
+    public String new3() {
+        return "simpleide/new3";
+    }
+    @GetMapping("/new4")
+    public String new4() {
+        return "simpleide/new4";
     }
 }
