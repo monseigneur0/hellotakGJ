ad3b2251c015d32e4489f5fac80626195f2c8c5a
monseigneur0 <82815356+monseigneur0@users.noreply.github.com>
2022-12-18 16:50:48 +0900

    organize and google adsence

--- src/main/java/hello8/hellotakGJ/web/HelloController.java
+++ src/main/java/hello8/hellotakGJ/web/HelloController.java
@@ -1,8 +1,10 @@
 package hello8.hellotakGJ.web;
 
+import hello8.hellotakGJ.web.dto.HolaResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,6 +13,15 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!!");
         return "hello";
+    }
+    @GetMapping("/hola")
+    public String hola() {
+        return "hola";
+    }
 
+    @GetMapping("/hola/dto")
+    public HolaResponseDto holaDto(@RequestParam("name") String name,
+                                   @RequestParam("amount") int amount) {
+        return new HolaResponseDto(name, amount);
     }
 }
