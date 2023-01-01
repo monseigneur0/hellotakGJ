b1bcf62958f9d7ad7dba7f881bdb4adfc36ee263
monseigneur0 <82815356+monseigneur0@users.noreply.github.com>
2023-01-01 18:45:27 +0900

    login update3

--- src/main/java/hello8/hellotakGJ/web/HomeController.java
+++ src/main/java/hello8/hellotakGJ/web/HomeController.java
@@ -1,16 +1,40 @@
 package hello8.hellotakGJ.web;
 
+import hello8.hellotakGJ.domain.member.LoginMember;
+import hello8.hellotakGJ.domain.member.LoginMemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class HomeController {
 
-    @RequestMapping("/home")
+    private final LoginMemberRepository memberRepository;
+
+    //@RequestMapping("/home")
     public String home() {
         log.info("home controller");
         return "home";
+    }
+
+    @GetMapping("/home")
+    public String homeLogin(
+            @CookieValue(name = "memberId", required = false) Long memberId,
+            Model model) {
+        if (memberId == null) {
+            return "home";
+        }
+//로그인
+        LoginMember loginMember = memberRepository.findById(memberId); if (loginMember == null) {
+            return "home";
+        }
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
--- src/main/java/hello8/hellotakGJ/web/LoginController.java
+++ src/main/java/hello8/hellotakGJ/web/LoginController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -22,7 +24,8 @@ public class LoginController {
     public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
         return "login/loginForm";
     }
-    @PostMapping("/login")
+
+    //@PostMapping("/login")
     public String login(@Valid @ModelAttribute LoginForm form, BindingResult
             bindingResult) {
         if (bindingResult.hasErrors()) {
@@ -37,5 +40,38 @@ public class LoginController {
         }
         //로그인 성공 처리 TODO
         return "redirect:/";
+    }
+
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult
+            bindingResult, HttpServletResponse response) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+        LoginMember loginMember = loginService.login(form.getLoginId(),
+                form.getPassword());
+        log.info("login? {}", loginMember);
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+        //로그인 성공 처리
+//쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId",
+                String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
+        return "redirect:/home";
+
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
